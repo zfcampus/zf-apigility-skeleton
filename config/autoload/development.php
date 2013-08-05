b46883f61d4b039843fc4d09babfcc3f080eeb47
Ralph Schindler <ralph@ralphschindler.com>
2013-08-05 12:51:19 -0500

    Removed diagnostics route

--- config/autoload/development.php
+++ config/autoload/development.php
@@ -1,20 +1,5 @@
 <?php
 return array(
-    // Eh, not a fan of having to do this
-    'router' => array(
-        'routes' => array(
-            'zftool-diagnostics' => array(
-                'type'  => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route' => '/zftool/diagnostics',
-                    'defaults' => array(
-                        'controller' => 'ZFTool\Controller\Diagnostics',
-                        'action'     => 'run'
-                    )
-                )
-            )
-        )
-    ),
     'view_manager' => array(
         'strategies' => array(
             'ViewJsonStrategy',
