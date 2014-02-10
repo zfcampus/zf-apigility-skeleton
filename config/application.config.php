3a8238039959379db363220357d7476e3b94a38f
Matthew Weier O'Phinney <matthew@zend.com>
2014-02-10 13:13:42 -0600

    Removes skeleton dev mode for zf-development-mode
    - This is a better solution, as the development mode functionality has a lot of
      general-purpose application, vs. being hidden in the skeleton.

--- config/application.config.php
+++ config/application.config.php
@@ -8,6 +8,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'ZF\DevelopmentMode',
         'ZF\Apigility',
         'ZF\Apigility\Provider',
         'ZF\Apigility\Welcome',
