3e0e24dcba622b5c33e5a13179b05c39da07a4e1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-02-13 21:47:18 +0700

    update copyright year to 2014

--- config/autoload/zfconfig.global.php
+++ config/autoload/zfconfig.global.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 return array(
