973ef3d79569ee73d015d90ab14fe864d7dc8d2e
Dominic Watson <domwatson@live.co.uk>
2014-07-16 00:48:25 +0200

    #68 - Added config and module caching. development.config.php disables it

--- config/application.config.php
+++ config/application.config.php
@@ -33,6 +33,11 @@ return array(
             './module',
             './vendor'
         ),
-        'config_glob_paths' => array('config/autoload/{,*.}{global,local}.php')
+        'config_glob_paths' => array('config/autoload/{,*.}{global,local}.php'),
+        'config_cache_key' => 'application.config.cache',
+        'config_cache_enabled' => true,
+        'module_map_cache_key' => 'application.module.cache',
+        'module_map_cache_enabled' => true,
+        'cache_dir' => 'data/cache/',
     )
 );
