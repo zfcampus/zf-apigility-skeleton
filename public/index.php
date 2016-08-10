cce379fd8c059f8c239242e6dc68b3844800aa5a
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-10 16:32:24 -0500

    Improve bootstrapping
    - Do not need to munge the development.config.php; it already uses
      realpath().
    - Better error messages when unable to autoload.
    - Remove APPLICATION_PATH definition; not used or required.
    - Import classes used.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@
  * @copyright Copyright (c) 2014-2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
+use Zend\Stdlib\ArrayUtils;
+use ZF\Apigility\Application;
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -17,35 +20,24 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 
 if (! file_exists('vendor/autoload.php')) {
     throw new RuntimeException(
-        'Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.'
+        "Unable to load application.\n"
+        . "- Type `composer install` if you are developing locally.\n"
+        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+        . "- Type `docker-compose run zf composer install` if you are using Docker.\n"
     );
 }
 
 // Setup autoloading
 include 'vendor/autoload.php';
 
-if (! defined('APPLICATION_PATH')) {
-    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
-}
+$appConfig = include 'config/application.config.php';
 
-$appConfig = include APPLICATION_PATH . '/config/application.config.php';
-
-if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
-    $appConfig = Zend\Stdlib\ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/development.config.php');
-}
-
-// Some OS/Web Server combinations do not glob properly for paths unless they
-// are fully qualified (e.g., IBM i). The following prefixes the default glob
-// path with the value of the current working directory to ensure configuration
-// globbing will work cross-platform.
-if (isset($appConfig['module_listener_options']['config_glob_paths'])) {
-    foreach ($appConfig['module_listener_options']['config_glob_paths'] as $index => $path) {
-        if ($path !== 'config/autoload/{,*.}{global,local}.php') {
-            continue;
-        }
-        $appConfig['module_listener_options']['config_glob_paths'][$index] = getcwd() . '/' . $path;
-    }
+if (file_exists('config/development.config.php')) {
+    $appConfig = ArrayUtils::merge(
+        $appConfig,
+        include 'config/development.config.php'
+    );
 }
 
 // Run the application!
-ZF\Apigility\Application::init($appConfig)->run();
+Application::init($appConfig)->run();
