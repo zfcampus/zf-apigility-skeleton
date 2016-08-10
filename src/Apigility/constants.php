78ff5b56b495f0ff00fbfe29a3315e26d2dbb6b4
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-10 11:00:28 -0500

    Define Apigility\VERSION constant

---
+++ src/Apigility/constants.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace Apigility;
+
+const VERSION = '1.4.0dev';
