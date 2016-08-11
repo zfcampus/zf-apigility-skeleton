ab348d93e73a421214a6a0ce2a7b5be69ab53b7d
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-11 12:49:24 -0500

    Reference THIS repo, not the ZF skeleton app

--- module/Application/test/Controller/ZZIndexControllerDevModeTest.php
+++ module/Application/test/Controller/ZZIndexControllerDevModeTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      http://github.com/zfcampus/zf-apigility-skeleton for the canonical source repository
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
